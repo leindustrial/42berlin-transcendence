9b3264654f14a7c6330bb95e739c30d9241defff
sepber <selina@pradel.de>
2024-06-18 19:45:24 +0200

    created Class Profile in users app models.py, added a one-to-one-link to the User from Django User Model and registered it in the admin.py, did not makemigrations and did not migrate the changes to db

--- django_backend/users/models.py
+++ django_backend/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	display_name = models.CharField(max_length=12, blank=True)
+
+# Catch changes on Django Built-In User Model with receiver signal
+# When a User is created in the User Model, a user will be created in the Profile model
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
