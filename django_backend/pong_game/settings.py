b06f132227af31c815a367d0a471452487ca05a5
sepber <selina@pradel.de>
2024-06-19 18:11:51 +0200

    extended the Built-In User Model with OneToOneField User to user in Profile model. Profile model contains so far a display name and the avatar

--- django_backend/pong_game/settings.py
+++ django_backend/pong_game/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- django_backend/users/signals.py
+++ django_backend/users/signals.py
@@ -1,20 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from .models import Profile
 
-# Create your models here.
-class Profile(models.Model):
-	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-	display_name = models.CharField(max_length=12, blank=True)
-	avatar = models.ImageField(null=True, blank=True, upload_to="avatars/")
-
-	# for admin area
-	def __str__(self):
-		return str(self.user)
-
-# Catch changes on Django Built-In User Model with receiver signal
-# When a User is created in the User Model, a user will be created in the Profile model
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
 	if created:
@@ -28,3 +16,4 @@ def save_user_profile(sender, instance, **kwargs):
 	if not hasattr(instance, 'profile'):
 		Profile.objects.create(user=instance)
 	instance.profile.save()
+
