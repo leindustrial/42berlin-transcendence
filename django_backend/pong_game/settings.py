663d3be4e5ced3e06d85c7b3d6827f4df409b6c1
Lola <lola@42berlin-tech1.local>
2024-06-22 16:09:40 +0200

    added language support, draft version

--- django_backend/pong_game/settings.py
+++ django_backend/pong_game/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'en-us'
 USE_I18N = True
 
 LANGUAGES = [
