8c65f9b08258a7e6bbec3b282c59656b65a74529
arash039 <shojaa.arash@gmail.com>
2024-07-16 02:28:14 +0200

    online tournament

--- django_backend/game/urls.py
+++ django_backend/game/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path
 from . import views
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),  # Include i18n URL for language setting
@@ -9,4 +8,6 @@ urlpatterns = [
     path('two-pl-game/', views.two_pl_game, name='two_pl_game'),
     path('four-pl-game/', views.four_pl_game, name='four_pl_game'),
     path('offline-game/', views.offline_game, name='offline_game'),
+    path('tournament/', views.tournament, name='tournament'),
+    re_path(r'tour_game/(?P<session_id>[0-9a-fA-F-]+)/$', views.tour_game, name='tour_game'),
 ]
