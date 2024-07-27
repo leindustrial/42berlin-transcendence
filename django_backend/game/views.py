d367e85485f7505cb1f8d4ec0b9c9adc93466b68
Lola Le <hey@leindustrial.ru>
2024-07-27 07:29:57 +0200

    working on a single page app

--- django_backend/game/views.py
+++ django_backend/game/views.py
@@ -21,40 +21,43 @@ def set_language(request):
 
 # Create your views here.
 
-def hello(request):
-	return render(request, 'game/hello.html')
-
-def offline_game(request):
-    return render(request, 'game/offline-game.html')
-
-def offline_tour(request):
-    return render(request, 'game/offline-tour.html')
-
-@login_required
-def game_start(request):
+def get_started(request):
 	return render(request, 'game/index.html', {})
 
-def two_pl_game(request):
-    if request.user.is_authenticated:
-        return render(request, 'game/two-pl-game.html')
-    else:
-        return redirect('/')
+# def hello(request):
+# 	return render(request, 'game/hello.html')
 
-def four_pl_game(request):
-    if request.user.is_authenticated:
-        return render(request, 'game/four-pl-game.html')
-    else:
-        return redirect('/')
+# def offline_game(request):
+#     return render(request, 'game/offline-game.html')
+
+# def offline_tour(request):
+#     return render(request, 'game/offline-tour.html')
+
+# @login_required
+# def game_start(request):
+# 	return render(request, 'game/index.html', {})
+
+# def two_pl_game(request):
+#     if request.user.is_authenticated:
+#         return render(request, 'game/two-pl-game.html')
+#     else:
+#         return redirect('/')
+
+# def four_pl_game(request):
+#     if request.user.is_authenticated:
+#         return render(request, 'game/four-pl-game.html')
+#     else:
+#         return redirect('/')
     
-def tournament(request):
-    if request.user.is_authenticated:
-        return render(request, 'game/tour.html')
-    else:
-        return redirect('/')
+# def tournament(request):
+#     if request.user.is_authenticated:
+#         return render(request, 'game/tour.html')
+#     else:
+#         return redirect('/')
 
-@login_required
-def tour_game(request, session_id=None):
-    if request.user.is_authenticated:
-        return render(request, 'game/tour-game.html')
-    else:
-        return redirect('/')
+# @login_required
+# def tour_game(request, session_id=None):
+#     if request.user.is_authenticated:
+#         return render(request, 'game/tour-game.html')
+#     else:
+#         return redirect('/')
