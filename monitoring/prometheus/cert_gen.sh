a7dedcd8c2ebba846cafbcd8600d7f01898c1ca6
arash039 <shojaa.arash@gmail.com>
2024-07-29 14:03:14 +0200

    setuptools 72.0.0 fix

--- monitoring/prometheus/cert_gen.sh
+++ monitoring/prometheus/cert_gen.sh
@@ -14,5 +14,5 @@ echo "$SSL_KEY" > "$KEY_FILE"
 chmod 644 /etc/prometheus/certs/client.crt
 chmod 644 /etc/prometheus/certs/client.key
 
-/bin/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus #--storage.tsdb.retention.time=1d
+/bin/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --storage.tsdb.retention.time=1d
 
