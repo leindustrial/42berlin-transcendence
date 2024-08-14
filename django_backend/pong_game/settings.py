c5b6cd820cf28dbba203961876a664c8736d16e8
Lola Le <40656122+leindustrial@users.noreply.github.com>
2024-08-14 18:02:13 +0200

    testing

--- django_backend/pong_game/settings.py
+++ django_backend/pong_game/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-+_(o+y_bldmul6_5a^zn5v-rxueimk54h$dncyyy&gubdf88a6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
